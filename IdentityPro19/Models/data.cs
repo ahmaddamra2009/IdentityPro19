59fc4ac86aa9da24a4f08a0acdb5929aa31ed36b
Ahmad Damra <ahmeddamra_cs@hotmail.com>
2025-01-22 11:41:45 +0300

    IDDDDDDDDD

--- IdentityPro19/Models/data.cs
+++ IdentityPro19/Models/data.cs
@@ -2,5 +2,9 @@
 {
     public class data
     {
+        public int DataID { get; set; }
+        public int DataID1 { get; set; }
+        public int DataID2 { get; set; }
+        public int DataID3 { get; set; }
     }
 }
